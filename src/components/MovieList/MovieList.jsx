6a05e6a5dc1829788fe17bbcc84193489b36625e
lucas mendes <luckajw@gmail.com>
2025-01-27 19:24:52 -0300

    movendo os elementos html para os componentes

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,11 @@
+import { MovieCard } from '../MovieCard/MovieCard';
 import './MovieList.scss';
+import * as movies from '../../api/movies.json';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = () =>
+  <>
+    {movies.map(movie => (
+      <MovieCard key={movie.imdbId} movie={movie} />
+    ))}
+  </>
+);
